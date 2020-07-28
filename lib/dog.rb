6ca0e4ba968f58bd818b4fc8ef5098e8718a05a1
Victor Avila <avilavictor1570@gmail.com>
2020-07-28 02:59:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,16 +4,31 @@ class Dog
  
   def initialize(name)
     @name = name
-    @@all << self
+    self.save
   end
  
   def self.all
     @@all
   end
   
-  def self.print_all
-    @@all
+  def self.clear_all
+    @@all = []
   end
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def name
+    @name
+  end
+  
 end
     
     
